0e41c6ff9ac6845dea530cb388836f350df27ccd
Gareth <gareth.james@force24.co.uk>
2024-12-02 21:07:13 +0000

    stuck on day2

--- day2/gj/main.py
+++ day2/gj/main.py
@@ -10,6 +10,8 @@ def running_diff(list):
 
 diff_lists = [running_diff([int(num) for num in line.strip().split()]) for line in reports]
 
+#Part 1
+
 def safe(list):
     if any(item == 0 for item in list):
         return False
@@ -27,3 +29,21 @@ def safe(list):
 safe_lists = [safe(list) for list in diff_lists]
 
 print(safe_lists.count(True), "<< part 1")
+
+#Part 2
+
+def less_safe(list):
+    if safe(list):
+        return True
+    else:
+        for index, num in enumerate(list):
+            tempList = list[:]
+            tempList.pop(index)
+            if safe(tempList):
+                return True
+            else:
+                return False
+
+less_safe_lists = [less_safe(list) for list in diff_lists]
+
+print(less_safe_lists.count(True), "<< part 2")
