1d562894e89ea9cd71df784ded6d02faca482490
Gareth <gareth.james@force24.co.uk>
2024-12-02 22:25:36 +0000

    simplify for just part1

--- day2/gj/main.py
+++ day2/gj/main.py
@@ -25,22 +25,3 @@ def safe(line):
 safe_lists = [safe(line) for line in diff_lists]
 
 print(safe_lists.count(True), "<< part 1")
-
-#Part 2
-
-def less_safe(line):
-    if safe(line):
-        return True
-    else:
-        for index, num in enumerate(line):
-            tempLine = line[:]
-            tempLine.pop(index)
-            if safe(tempLine):
-                return True  
-    return False
-
-less_safe_lists = [less_safe(line) for line in diff_lists]
-
-print(less_safe_lists.count(True), "<< part 2")
-
-##I'm not sure what I did wrong on part 2, and I can't see it. Feedback appreciated
