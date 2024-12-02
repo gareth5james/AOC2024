3f4acb035340d9020ed12ab0bf9b4fc6a757d313
Gareth <gareth.james@force24.co.uk>
2024-12-02 21:52:31 +0000

    reword variables

--- day2/gj/main.py
+++ day2/gj/main.py
@@ -2,45 +2,44 @@
 
 reports = open("input.txt", "r")
 
-def running_diff(list):
-    if len(list) <= 1:
+def running_diff(line):
+    if len(line) <= 1:
         return []
     else:
-        return [list[1] - list[0]] + running_diff(list[1:])
+        return [line[1] - line[0]] + running_diff(line[1:])
 
 diff_lists = [running_diff([int(num) for num in line.strip().split()]) for line in reports]
 
 #Part 1
 
-def safe(list):
-    if any(item == 0 for item in list):
+def safe(line):
+    if any(num == 0 for num in line):
         return False
-    elif any(abs(item) > 3 for item in list):
+    elif any(abs(num) > 3 for num in line):
         return False
-    elif all(item > 0 for item in list) or all(item < 0 for item in list):
+    elif all(num > 0 for num in line) or all(num < 0 for num in line):
         return True
     else:
         return False
     
-safe_lists = [safe(list) for list in diff_lists]
+safe_lists = [safe(line) for line in diff_lists]
 
 print(safe_lists.count(True), "<< part 1")
 
 #Part 2
 
-def less_safe(list):
-    if safe(list):
+def less_safe(line):
+    if safe(line):
         return True
     else:
-        for index, num in enumerate(list):
-            tempList = list[:]
-            tempList.pop(index)
-            if safe(tempList):
-                return True
-    
+        for index, num in enumerate(line):
+            tempLine = line[:]
+            tempLine.pop(index)
+            if safe(tempLine):
+                return True  
     return False
 
-less_safe_lists = [less_safe(list) for list in diff_lists]
+less_safe_lists = [less_safe(line) for line in diff_lists]
 
 print(less_safe_lists.count(True), "<< part 2")
 
