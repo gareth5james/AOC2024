8f9955a5f2c92036bfba897220d9fdd42f6eda7e
ScottNC <scottnc@hotmail.co.uk>
2024-12-05 16:55:53 +0000

    passed part 1

--- day2/snc/src/Safe.java
+++ day2/snc/src/Safe.java
@@ -1,6 +1,41 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static java.lang.Math.abs;
 
 public class Safe {
+    public static void main() {
+        Path path = Paths.get("day2/snc/input.txt");
+
+        try {
+
+            String[] lines = Files.readAllLines(path).toArray(new String[0]);
+
+            int result1 = 0;
+
+            for (String line : lines) {
+                String[] numberStr = line.split(" ");
+                Integer[] numbers = new Integer[numberStr.length];
+
+                for (int j = 0; j < numberStr.length; j++) {
+                    numbers[j] = Integer.parseInt(numberStr[j]);
+                }
+
+                if (isSafe(numbers))
+                    result1++;
+            }
+
+            // Part 1
+            System.out.println("Part 1: " + result1);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     static boolean isSafe(Integer[] arr) {
 
         boolean alwaysPositive = false;
