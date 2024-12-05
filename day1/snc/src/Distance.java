52fa1f7ff75382f22aa69b466765c40f4227993f
ScottNC <scottnc@hotmail.co.uk>
2024-12-05 15:02:29 +0000

    finds total of distances between arrays

--- day1/snc/src/Distance.java
+++ day1/snc/src/Distance.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
 public class Distance {
     static int findDistance(Integer[] arr1, Integer[] arr2) {
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
         int total = 0;
         for (int i = 0; i < arr1.length; i++) {
-            total += arr1[i] - arr2[i];
+            total += abs(arr1[i] - arr2[i]);
         }
         return total;
     }
--- day1/snc/test/DistanceTest.java
+++ day1/snc/test/DistanceTest.java
@@ -8,5 +8,17 @@ class DistanceTest {
         assertEquals(2, Distance.findDistance(new Integer[]{9}, new Integer[]{7}));
         assertEquals(5, Distance.findDistance(new Integer[]{6, 9}, new Integer[]{3, 7}));
         assertEquals(6, Distance.findDistance(new Integer[]{1, 3, 6, 9}, new Integer[]{1, 2, 3, 7}));
+
+        assertEquals(6, Distance.findDistance(new Integer[]{1, 6, 3, 9}, new Integer[]{1, 2, 3, 7}));
+    }
+
+    @Test
+    void wrongOrder() {
+        assertEquals(11, Distance.findDistance(new Integer[]{8, 5, 9}, new Integer[]{7, 3, 1}));
+    }
+
+    @Test
+    void bothWays() {
+        assertEquals(15, Distance.findDistance(new Integer[]{8, 5, 9, 10}, new Integer[]{7, 3, 1, 14}));
     }
 }
