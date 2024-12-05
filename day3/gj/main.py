dbd628c3c3f3ce12318b47140b5da0efdc698c0a
Gareth <gareth.james@force24.co.uk>
2024-12-05 08:55:53 +0000

    git conflict

--- day3/gj/main.py
+++ day3/gj/main.py
@@ -1,13 +1,17 @@
+import re
+
+from re import findall
+
 total1 = total2 = 0
-    enabled = True
-    data = open("input.txt").read()
+enabled = True
+data = open("input.txt").read()
 
-    for a, b, do, dont in findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", data):
-        if do or dont:
-            enabled = bool(do)
-        else:
-            x = int(a) * int(b)
-            total1 += x
-            total2 += x * enabled
+for a, b, do, dont in findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", data):
+    if do or dont:
+        enabled = bool(do)
+    else:
+        x = int(a) * int(b)
+        total1 += x
+        total2 += x * enabled
 
-    print(total1, total2)
+print(total1, total2)
