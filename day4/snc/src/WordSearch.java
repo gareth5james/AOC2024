d3a87e1312e953adf5b890565b43e60db89b278e
ScottNC <scottnc@hotmail.co.uk>
2024-12-09 18:09:15 +0000

    passes day 4 part 1

---
+++ day4/snc/src/WordSearch.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WordSearch {
+
+    private static String word;
+
+    public static void main() {
+        Path path = Paths.get("day4/snc/input.txt");
+
+
+        try {
+            String[] lines = Files.readAllLines(path).toArray(new String[0]);
+
+            // Part 1
+            Integer result1 = findXmas(lines);
+            System.out.println("Part 1: " + result1);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static int isWord(int i, int j, int x, int y, String[] arr) {
+        for (int k = 1; k < word.length(); k++) {
+            i += x;
+            j += y;
+
+            if (arr[i].charAt(j) != word.charAt(k))
+                return 0;
+        }
+        return 1;
+    }
+
+    static Integer findXmas (String[] arr) {
+        word = "XMAS";
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length(); j++) {
+                if (arr[i].charAt(j) == word.charAt(0)) {
+                    if (i < arr.length - word.length() + 1) {
+                        if (j < arr[0].length() - word.length() + 1) {
+                            count += isWord(i, j, 1, 1, arr);
+                        }
+
+                        if (j >= word.length() - 1) {
+                            count += isWord(i, j, 1, -1, arr);
+                        }
+
+                        count += isWord(i, j, 1, 0, arr);
+                    }
+
+                    if (i >= word.length() - 1) {
+                        if (j < arr[0].length() - word.length() + 1) {
+                            count += isWord(i, j, -1, 1, arr);
+                        }
+
+                        if (j >= word.length() - 1) {
+                            count += isWord(i, j, -1, -1, arr);
+                        }
+
+                        count += isWord(i, j, -1, 0, arr);
+                    }
+
+                    if (j < arr[0].length() - word.length() + 1) {
+                        count += isWord(i, j, 0, 1, arr);
+                    }
+
+                    if (j >= word.length() - 1) {
+                        count += isWord(i, j, 0, -1, arr);
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ day4/snc/test/WordSearchTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WordSearchTest {
+    @Test
+    void findXmas() {
+        assertEquals(1, WordSearch.findXmas(new String[]{"XMAS", "XXXX", "XXXX", "XXXX"}));
+        assertEquals(2, WordSearch.findXmas(new String[]{"XMAS", "MXXX", "AXXX", "SXXX"}));
+    }
+
+    @Test
+    void findXmas2() {
+        assertEquals(1, WordSearch.findXmas(new String[]{"SHHH", "HAHH", "HHMH", "HHHX"}));
+    }
+
+    @Test
+    void findXmas3() {
+        assertEquals(18, WordSearch.findXmas(new String[]{"MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM",
+                "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"}));
+    }
+
+}
