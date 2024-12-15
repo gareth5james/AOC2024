81bc69aaffa505d5f314ce5b991bd50eb3528c88
ScottNC <scottnc@hotmail.co.uk>
2024-12-15 21:29:06 +0000

    initial test for addition

--- day7/snc/test/OperatorTest.java
+++ day7/snc/test/OperatorTest.java
@@ -5,9 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OperatorTest {
     @Test
     void calculate() {
-
         String bridge = "190: 10 19";
-
         assertEquals(190, Operator.calculate(bridge));
+    }
+
+    @Test
+    void calculate2() {
+        String bridge = "29: 10 19";
+        assertEquals(29, Operator.calculate(bridge));
     }
 }
