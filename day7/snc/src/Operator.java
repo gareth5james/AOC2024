aba8642a14c8199a5182f349c09bca57f3727cc1
ScottNC <scottnc@hotmail.co.uk>
2024-12-15 22:20:41 +0000

    finished day 7

--- day7/snc/src/Operator.java
+++ day7/snc/src/Operator.java
@@ -13,20 +13,25 @@ public class Operator {
             String[] lines = Files.readAllLines(path).toArray(new String[0]);
 
             long result1 = 0;
+            long result2 = 0;
 
             for (String line: lines) {
-                result1 += calculate(line);
+                result1 += calculate(line, 2);
+                result2 += calculate(line, 3);
             }
 
             // Part 1
             System.out.println("Part 1: " + result1);
+
+            // Part 2
+            System.out.println("Part 2: " + result2);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    static long calculate (String bridge) {
+    static long calculate (String bridge, int power) {
         String[] values = bridge.split(": ");
 
         long finalValue = Long.parseLong(values[0]);
@@ -38,16 +43,21 @@ public class Operator {
 
         int operators = variables.length - 1;
 
-        for (int i = 0; i < Math.pow(2, operators); i++) {
-            String binary = String.format("%" + operators + "s", Integer.toBinaryString(i));
+        for (int i = 0; i < Math.pow(power, operators); i++) {
+            String binary = String.format("%" + operators + "s", Integer.toString(i, power));
 
             long lastValue = variables[0];
 
             for (int j = 0; j < operators; j++) {
                 if (binary.charAt(j) == '1')
                     lastValue += variables[j + 1];
+                else if (binary.charAt(j) == '2') {
+                    String next = lastValue + String.valueOf(variables[j + 1]);
+                    lastValue = Long.parseLong(next);
+                }
                 else
                     lastValue *= variables[j + 1];
+
             }
 
             if (lastValue == finalValue)
--- day7/snc/test/OperatorTest.java
+++ day7/snc/test/OperatorTest.java
@@ -6,42 +6,60 @@ public class OperatorTest {
     @Test
     void calculate() {
         String bridge = "190: 10 19";
-        assertEquals(190, Operator.calculate(bridge));
+        assertEquals(190, Operator.calculate(bridge, 2));
     }
 
     @Test
     void calculate2() {
         String bridge = "29: 10 19";
-        assertEquals(29, Operator.calculate(bridge));
+        assertEquals(29, Operator.calculate(bridge, 2));
     }
 
     @Test
     void calculate3() {
         String bridge = "39: 10 19";
-        assertEquals(0, Operator.calculate(bridge));
+        assertEquals(0, Operator.calculate(bridge, 2));
     }
 
     @Test
     void calculate4() {
         String bridge = "56: 10 19 27";
-        assertEquals(56, Operator.calculate(bridge));
+        assertEquals(56, Operator.calculate(bridge, 2));
     }
 
     @Test
     void calculate5() {
         String bridge = "3267: 81 40 27";
-        assertEquals(3267, Operator.calculate(bridge));
+        assertEquals(3267, Operator.calculate(bridge, 2));
     }
 
     @Test
     void calculate6() {
         String bridge = "292: 11 6 16 20";
-        assertEquals(292, Operator.calculate(bridge));
+        assertEquals(292, Operator.calculate(bridge, 2));
     }
 
     @Test
     void calculate7() {
         String bridge = "21037: 9 7 18 13";
-        assertEquals(0, Operator.calculate(bridge));
+        assertEquals(0, Operator.calculate(bridge, 2));
+    }
+
+    @Test
+    void calculate8() {
+        String bridge = "156: 15 6";
+        assertEquals(156, Operator.calculate(bridge, 3));
+    }
+
+    @Test
+    void calculate9() {
+        String bridge = "7290: 6 8 6 15";
+        assertEquals(7290, Operator.calculate(bridge, 3));
+    }
+
+    @Test
+    void calculate10() {
+        String bridge = "7290: 6 8 6 15";
+        assertEquals(0, Operator.calculate(bridge, 2));
     }
 }
