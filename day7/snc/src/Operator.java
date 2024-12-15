87d894ac7d87c9d063fc18d97f48d96ccdf4e383
ScottNC <scottnc@hotmail.co.uk>
2024-12-15 22:04:19 +0000

    passed day 7 part 1

--- day7/snc/src/Operator.java
+++ day7/snc/src/Operator.java
@@ -1,10 +1,35 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Operator {
-    static Integer calculate (String bridge) {
+
+    public static void main() {
+        Path path = Paths.get("day7/snc/input.txt");
+
+        try {
+            String[] lines = Files.readAllLines(path).toArray(new String[0]);
+
+            long result1 = 0;
+
+            for (String line: lines) {
+                result1 += calculate(line);
+            }
+
+            // Part 1
+            System.out.println("Part 1: " + result1);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static long calculate (String bridge) {
         String[] values = bridge.split(": ");
 
-        int finalValue = Integer.parseInt(values[0]);
+        long finalValue = Long.parseLong(values[0]);
 
         Integer[] variables = Arrays.stream(values[1].split(" "))
                 .map(Integer::valueOf)
@@ -16,7 +41,7 @@ public class Operator {
         for (int i = 0; i < Math.pow(2, operators); i++) {
             String binary = String.format("%" + operators + "s", Integer.toBinaryString(i));
 
-            int lastValue = variables[0];
+            long lastValue = variables[0];
 
             for (int j = 0; j < operators; j++) {
                 if (binary.charAt(j) == '1')
